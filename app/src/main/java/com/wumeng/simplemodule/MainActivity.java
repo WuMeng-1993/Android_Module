97b01c20fda50046bb7a8f1460dd2500ec96eb6e
WuMeng <541345568@qq.com>
2020-05-03 11:25:16 +0800

    第一次提交项目

---
+++ app/src/main/java/com/wumeng/simplemodule/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wumeng.simplemodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author WuMeng
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
